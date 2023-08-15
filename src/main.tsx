a7ea4c050eb03c6963ae1075ec13e7599e3c8e38
Amanda <amandaannelewis@gmail.com>
2023-08-15 16:26:10 +0000

    added react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { Root } from "./routes/Root.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ErrorPage } from "./components/ErrorPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/routes/Root.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../components/Header";
+import { Home } from "../components/Home";
+import { Footer } from "../components/Footer";
+
+export const Root = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
+};
